8231347603a405da0740914ceff1d0bbc8386437
Samyuktha <120277.samyukthab.gbkm@gmail.com>
2025-08-01 23:59:57 +0530

    to find which day from the given date month year

---
+++ calender.java
@@ -0,0 +1,28 @@
+import java.util.Calendar;
+import java.util.Scanner;
+public class calender {
+
+    public static String getDay(int month, int day, int year) {
+        Calendar c = Calendar.getInstance();
+        c.set(year, month - 1, day); 
+
+        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+        
+        // Array to map Calendar days to strings
+        String[] days = { "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", 
+                          "THURSDAY", "FRIDAY", "SATURDAY" };
+
+        return days[dayOfWeek - 1]; 
+    }
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter month (1-12):");
+        int month = s.nextInt();
+        System.out.println("enter day(1-7):");
+        int day = s.nextInt();
+        System.out.println("enter year:");
+        int year = s.nextInt();
+        System.out.println("the day is :"+getDay(month, day, year)); 
+    }
+}
