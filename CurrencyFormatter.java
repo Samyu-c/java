a9cf2848402fad63ce43b597d764ebe39573b9a4
Samyuktha <samyukthab126@gmail.com>
2025-08-10 23:13:12 +0530

    Add CurrencyFormatter.java

---
+++ CurrencyFormatter.java
@@ -0,0 +1,20 @@
+import java.text.*;
+import java.util.*;
+
+public class CurrencyFormatter {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        String us=NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        Locale localindia= new Locale("en","IN");
+        String india=NumberFormat.getCurrencyInstance(localindia).format(payment);
+        String china=NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france=NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);  
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
